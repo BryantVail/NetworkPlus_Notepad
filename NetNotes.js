544f6e69e97cfe956a37d41b538ae8ae75a7c6ae
Bryant Vail <Bryanttv@outlook.com>
2018-04-13 17:03:41 -0400

    inti

--- NetNotes.js
+++ NetNotes.js
@@ -1,57 +1,27 @@
-// 3-1_Coax_CablingAndTopology
+// 3-2_UTP-STP-cabling_CablingAndTopology
 /*
 Video Notes below:
-This video explores 
-	the big three of coaxial cabling - 
-		RG-8 and RG-58, 
-			the old 
-		and 
-		RG-6 cabling
-			widely used 
+In this video, 	you will explore 
+	the different types and categories of 
+	twisted pair cabling used in computer networking.
 */
 
-Coax: {
-	"Etymology":
-		"Co-axial, or having 2 conductors around a single axis",
-	"cable standard": [
-			`RG-${num}`,
-			"Radio Grade (RG) rating",
-				"Ohm rating":
-					"measured in impedance",
-	],
-	"types": [
-		"rg-8",
-		"rg-58",
-			"details":50 Ohm impedance
-			"connector":
-				"BNC connector",
-				"T-Connector":
-					"used to 'daisy chain'",
-				
-			
-				
-		"rg-6":
-			"F-Connector":
-				"screw on connector"
-			"used today"
-	]
-				
-	"cable technology":[
-		center:
-			:conductor",
-		"2ndLayer":
-			insulation
-			"looks like white plastic"
-		"3rdLayer":
-			another conductor
-		"4thLayer":
-			insulation
-			"sheething"
-				
-			outer insulation
-	]//end cable technology
-}//end coax general
-  
+"UTP":
+	"unshielded Twisted pair"
+	"description":
+		"twisted pair reduces interference by sending signals on a pair that are twisted"
+		"kevlar":
+			"
+
+
+
+
+
+
+
+
+
+
   
   
   
