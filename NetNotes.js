b41bb3ccd1fae8ee2cd00b3aeeb676f6c72d908e
Bryant Vail <Bryanttv@outlook.com>
2018-04-13 16:02:50 -0400

    init

--- NetNotes.js
+++ NetNotes.js
@@ -1,7 +1,27 @@
 // 3-1_Coax_CablingAndTopology
+/*
+Video Notes below:
+This video explores 
+	the big three of coaxial cabling - 
+		RG-8 and RG-58, 
+			the old 
+		and 
+		RG-6 cabling
+			widely used 
+*/
 
 Coax: {
- 
+	"Coaxial":
+		
+	"cable standard":
+			`RG-${num}`,
+				
+	"cable technology":
+		center:
+			conductor
+			insulation
+			outer insulation
+		
 }
   
   
