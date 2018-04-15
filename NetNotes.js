b2210a697691f230a602f26c38185929a81f7ab9
bxvail <BryantVail@gmail.com>
2018-04-15 12:59:01 -0400

    listing

--- NetNotes.js
+++ NetNotes.js
@@ -6,12 +6,63 @@ In this video, 	you will explore
 	twisted pair cabling used in computer networking.
 */
 
-"UTP":
-	"unshielded Twisted pair"
-	"description":
-		"twisted pair reduces interference by sending signals on a pair that are twisted"
+let cableTechnologies = [
+	{	
+		"technologyName":"UTP",
+		"etymology":
+			"unshielded Twisted pair",
+		"description":
+			"twisted pair reduces interference by sending signals on a pair that are twisted",
 		"kevlar":
-			"
+			"used to provide 'linear' strength, to be able to pull the line through conduit etc.",	
+		"categories":[
+			{
+				"name":"cat3", 
+				"capacity": "10mbps"
+			},
+			{
+				"name":"cat5",
+				"capacity": "100mbps",
+				"notes":[
+					"updated to cat5e shortly after the standard was released",
+					"cat5, generally, when referred to, people are referring to cat5e"
+				]
+			},
+			{
+				"name":"cat5e",
+				"capacity": "100mbps",
+				"notes":[
+					"cat5 updated to cat5e shortly after the standard was released",
+					"cat5e, is generally what is being referred to when people reference cat5 "
+				]
+			},
+			{
+				"name":"cat6",
+				"capacity":
+				[
+					"1000mbps",
+					"1gbps"
+				],
+				"notes": [
+					"can run 10gb nets on 'old' cat 6, but the length capacity was very short",
+					"standard network cable run can be up to 100 meters",
+					"to run 10gb run w/traditional cat6, it would be more like 55 meters"
+				]
+			},
+			{
+				"name":"cat6a",
+				"capacity": "10gbps",
+				"notes": [
+					"can run 10gb nets on 'old' cat 6, but the length capacity was very short",
+					"standard network cable run can be up to 100 meters",
+					"to run 10gb run w/traditional cat6, it would be more like 55 meters",
+					"compared to older categories/standards, cat6 & cat6e have noticibly higher gauge wire"
+				]
+			}
+		]//end categories
+	},//end UTP	
+
+	
 
 
 
