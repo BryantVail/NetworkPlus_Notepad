0b26c34b39e954fa04b28c64882db8d0d991dd5f
Bryant Vail <Bryanttv@outlook.com>
2018-04-16 15:27:18 -0400

    done

--- 10Base2.js
+++ 10Base2.js
@@ -1,3 +1,13 @@
 //3-2_EarlyEthernet_CablingAndTopology
 
-let 10Base2 = 
+let EarlyEthernet = {
+	"10Base2":[
+		"capacity":"up to 30 devices",
+		"lengthOfRun":"up to 200meters, actually 185mbps",
+		"bandwidth":"10mbps",
+		"terminators":"terminators needed to resist 'reflection' of the signal at the end of the run",
+		"BNC connector":"twist lock connector"
+		
+	]
+  
+}
