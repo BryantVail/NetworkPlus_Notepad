d426ecae3d1a75d9c67b5e53df566b30a010ecf2
Bryant Vail <Bryanttv@outlook.com>
2018-04-16 15:28:11 -0400

    csma/cd

--- 10Base2.js
+++ 10Base2.js
@@ -6,8 +6,9 @@ let EarlyEthernet = {
 		"lengthOfRun":"up to 200meters, actually 185mbps",
 		"bandwidth":"10mbps",
 		"terminators":"terminators needed to resist 'reflection' of the signal at the end of the run",
-		"BNC connector":"twist lock connector"
-		
+		"BNC connector":"twist lock connector",
+		"CSMA/CD":"Carrier Sense Multiple Access/ Collision Detection",
+			
 	]
   
 }
