8fb8537e6da8b4ff5ad10b3b32f58aa1bd0ee9fe
Bryant Vail <Bryanttv@outlook.com>
2018-04-16 15:37:08 -0400

    done

--- NetNotes.js
+++ NetNotes.js
@@ -5,7 +5,26 @@ In this video, you will look at how
 	coax-based Ethernet got transformed into Ethernet of Unshielded Twisted Pair.
 */
 
-
+let theDaddyOfEthernet = {
+	"TokenRing":{
+		"company":"IBM",
+		"circa":"late 1980's ",
+		"description":{
+			"msau":"multi-station access unit"
+		},
+		"problems":[
+			"proprietary",
+			"Expensive"
+		]
+	}
+	"10BaseT":{
+		"NickName":"bus in the box",
+		"Bandwidth":"10mbps",
+		"length Of Run":"100 meters from node & switch"
+		"Node Capacity":"1024",
+		"cabling":"twisted pair, build to run on cat3
+	}
+}
 
 
 
