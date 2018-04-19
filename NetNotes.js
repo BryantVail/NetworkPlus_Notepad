0fde93f6f1a92bdcc870df3267303b90043c1329
Bryant Vail <Bryanttv@outlook.com>
2018-04-19 14:16:49 -0400

    now

--- NetNotes.js
+++ NetNotes.js
@@ -6,11 +6,41 @@ In this video, you will look at how
 */
 
 let ModernEthernet = ModernEthernet || {};
-ModernEthernet= {
-	"10GBase"
+ModernEthernet= [
+	{
+		"name":"10GBaseT",
+		"transmissionMedium":"copper",
+		"transmissionStandards":[
+			{
+				"cableStandard":"CAT6",
+				"RunSwitchToNodeInMeters":55,
+			},
+			{
+				"cableStandard":"CAT6a",
+				"RunSwitchToNodeInMeters":100,
+			},
+		]	
+	},
+	{
+		"name":	"10GBaseSR",
+		"transmissionMedium":"fiber",
+		"transmissionStandard": {
+			"cableStandard":"multimode',
+			"runSwitchToNodeInMeters":[
+				26,
+				400
+			],
+		},
+	},//end 10GBaseSR
+	{
+		"name":	"10GBaseLR",
+		"transmissionMedium":"fiber",
+		"transmissionStandard":{
+			"cableStandard":
+		},
+	},
 
-
-
+]
 
 
 
