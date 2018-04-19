6947d67c918fe60cd33f7a351f21509bf0ebe682
Bryant Vail <Bryanttv@outlook.com>
2018-04-19 09:25:20 -0400

    init

--- NetNotes.js
+++ NetNotes.js
@@ -1,33 +1,13 @@
-// 3-3_TheDaddyOfEthernet-10BaseT_CablingAndTopology
+// 4-3_4-3_10GBase_ModernEthernet
 /*
 Video Notes below:
 In this video, you will look at how 
 	coax-based Ethernet got transformed into Ethernet of Unshielded Twisted Pair.
 */
 
-let theDaddyOfEthernet = {
-	"TokenRing":{
-		"company":"IBM",
-		"circa":"late 1980's ",
-		"description":{
-			"msau":"multi-station access unit"
-		},
-		"problems":[
-			"proprietary",
-			"Expensive"
-		]
-	}
-	"10BaseT":{
-		"NickName":"bus in the box",
-		"Bandwidth":"10mbps",
-		"length Of Run":"100 meters from node & switch"
-		"Node Capacity":"1024",
-		"cabling":"twisted pair, build to run on cat3"
-	}
-}
-
-
-
+let ModernEthernet = ModernEthernet || {};
+ModernEthernet= {
+	"10GBase"
 
 
 
