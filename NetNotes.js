9857f12ca1178cf6eedda8f2a2f4f135cbd372bd
Bryant Vail <Bryanttv@outlook.com>
2018-04-20 15:12:18 -0400

    end

--- NetNotes.js
+++ NetNotes.js
@@ -8,7 +8,27 @@ In this video, you will see how:
 let ModernEthernet = ModernEthernet || {};
 ModernEthernet= [
 	{
-			
+		"terms":[
+			{
+				"term":"GBIC",
+				"definition":"Gigabit Interface Converter; Designed to be moved in/out of switches to manage backbones that run at higher speeds coming down to a switch",
+			},
+			{
+				"term":"STP",
+				"definition":"Spanning Tree Protocol; 
+				"notes":[
+					"smart switch needed"
+				]
+			},
+			{
+				"term":"BLoop",
+				"definition": "Bridge Loop; looping switches with physical cabling can lead to packet loops.",
+				"notes":[
+					`${STP} automatically shuts down Bridge Loops (BLoops)`	
+				]
+			},
+		]//end terms
+		
 	},
 
 
