95e259cfb2e4deebe4aa095cdc756b8e6beecbb7
Bryant Vail <Bryanttv@outlook.com>
2018-04-20 13:00:06 -0400

    init

--- NetNotes.js
+++ NetNotes.js
@@ -1,56 +1,15 @@
-// 4-3_4-3_10GBase_ModernEthernet
+// 4-4_SwitchBackbones_ModernEthernet
 /*
 Video Notes below:
-In this video, you will look at how 
-	coax-based Ethernet got transformed into Ethernet of Unshielded Twisted Pair.
+In this video, you will see how: 
+	backbone switches are used to enable varying speed networks to interconnect.
 */
 
 let ModernEthernet = ModernEthernet || {};
 ModernEthernet= [
 	{
-		"name":"10GBaseT",
-		"transmissionMedium":"copper",
-		"transmissionStandards":[
-			{
-				"cableStandard":"CAT6",
-				"RunSwitchToNodeInMeters":55,
-			},
-			{
-				"cableStandard":"CAT6a",
-				"RunSwitchToNodeInMeters":100,
-			},
-		]	
+			
 	},
-	{
-		"name":	"10GBaseSR",
-		"transmissionMedium":"fiber",
-		"transmissionStandard": {
-			"cableStandard":"multimode',
-			"runSwitchToNodeInMeters":[
-				26,
-				400
-			],
-		},
-	},//end 10GBaseSR
-	{
-		"name":	"10GBaseLR",
-		"transmissionMedium":"fiber",
-		"transmissionStandard":{
-			"cableStandard":"single-mode",
-			"runSwitchToNodeInMeters":10,000, //10 Kilometers
-			"lightWavelengthInNanometers":1310	
-		},
-		
-	},
-	{
-		"name":"10GBaseER",
-		"transmissionMedium":"fiber",
-		"transmissionStandard":[
-				
-		]
-	},
-
-]
 
 
 
